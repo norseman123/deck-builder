f9b2d91d117d3b7ee3b64b7f811068f3923de7d4
norseman123 <134438786+norseman123@users.noreply.github.com>
2026-03-18 10:37:07 -0500

    Update data.js

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ const CLASSES = {
             { name: "Tripwire", time: 1, trap: { delay: 2, damage: 12 } },
             { name: "Minefield", time: 3, trap: { delay: 3, damage: 25 } },
             { name: "Shove", time: 1, damage: 4, delayEnemy: 2 },
-            { name: "Lure", time: 0, block: 8, delayEnemy: -1 }
+            { name: "Lure", time: 0, block: 8, delayEnemy: -1 },
             { name: "Tactical Retreat", time: 6, block: 25, draw: 1 },
         ]
     },
@@ -19,8 +19,8 @@ const CLASSES = {
             { name: "Flurry", time: 1, damage: 2, momentumDamage: 4 },
             { name: "Sprint", time: -1, draw: 1, selfDamage: 2 },
             { name: "Combo Finisher", time: 2, damage: 5, momentumDamage: 6 },
-            { name: "Blur", time: -2, block: 4, randomDiscard: 1 }
-            { name: "Unleash", time: 1, damage: 5, momentumDelay: 2}
+            { name: "Blur", time: -2, block: 4, randomDiscard: 1 },
+            { name: "Unleash", time: 1, damage: 5, momentumDelay: 2},
         ]
     },
     "The Stoic": {
@@ -31,9 +31,9 @@ const CLASSES = {
             { name: "Stand Ground", time: 1, block: 10, anchorPlayer: 2 },
             { name: "Shackles", time: 2, damage: 6, rootEnemy: 1 },
             { name: "Heavy Blow", time: 3, damage: 18 },
-            { name: "Immovable", time: 2, block: 20, anchorPlayer: 1 }
+            { name: "Immovable", time: 2, block: 20, anchorPlayer: 1 },
             { name: "Chain Grab", time: 1, damage: 8, pullEnemy: true },
-            { name: "Inhuman Suffering", time: 10, damage: 40}
+            { name: "Inhuman Suffering", time: 10, damage: 40},
         ]
     },
     "The Wanderer": {
@@ -44,7 +44,7 @@ const CLASSES = {
             { name: "Phase Shift", time: 0, shiftTimeline: true, draw: 1 },
             { name: "Paradox", time: 1, damage: 10, shiftTimeline: true },
             { name: "Ghost Block", time: 1, block: 12, shiftTimeline: true },
-            { name: "Cross-Strike", time: 2, damage: 8, hits: 2 }
+            { name: "Cross-Strike", time: 2, damage: 8, hits: 2 },
             { name: "Reality Crash", time: 2, collapseIntents: true, shiftTimeline: true },
         ]
     },
@@ -56,7 +56,7 @@ const CLASSES = {
             { name: "Roll the Bones", randomTime: [-1, 3], damage: 15 },
             { name: "Jackpot", time: 2, randomDamage: [5, 30] },
             { name: "Sleight of Hand", time: 0, draw: 2, randomDiscard: 1 },
-            { name: "Wild Bet", randomTime: [0, 2], block: 15, randomDiscard: 1 }
+            { name: "Wild Bet", randomTime: [0, 2], block: 15, randomDiscard: 1 },
         ]
     }
 };
